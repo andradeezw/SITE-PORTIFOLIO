7d4f6df6152e27f77b4e45dfbb5a959d967f7167
andradeezw <andradeekaua0@gmail.com>
2026-05-30 22:14:47 -0300

    Atualização do projeto

--- script.js
+++ script.js
@@ -17,3 +17,20 @@ formulario.addEventListener("submit", function(event){
     window.open(url, "_blank");
 
 });
+
+VANTA.DOTS({
+  el: "#vanta-bg",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xe0989,
+  color2: 0x20ffb0,
+  backgroundColor: 0x20202,
+  size: 1.80,
+  spacing: 22.00,
+  showLines: false
+})
